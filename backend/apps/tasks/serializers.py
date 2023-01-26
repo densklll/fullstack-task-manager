499f0102ee8997bb5762dfd98b6e51fcf386ed42
densklll <wqjasabim@mail.ru>
2023-01-26 10:51:20 +0300

    style(serializers): Review CRACO alias for tests (local dev)

--- backend/apps/tasks/serializers.py
+++ backend/apps/tasks/serializers.py
@@ -6,3 +6,5 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = '__all__' 
 # 2023-01-13: Capture JWT refresh timing vs axios queue (CI runner)
+
+# 2023-01-26: Review CRACO alias for tests (local dev)
