7bfa90baee7be9f88e17995e692fcd8b54c4b1b1
densklll <wqjasabim@mail.ru>
2023-01-30 05:01:48 +0300

    chore(celery): Describe DRF pagination cursor vs offset (CI runner)

--- backend/project/celery.py
+++ backend/project/celery.py
@@ -7,3 +7,5 @@ app = Celery('project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks() 
 # 2023-01-17: Describe swagger path in local README (local dev)
+
+# 2023-01-30: Describe DRF pagination cursor vs offset (CI runner)
