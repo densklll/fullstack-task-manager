d68cdb53d8f9530f07aa3e43116d3f3c98f5f2df
densklll <wqjasabim@mail.ru>
2023-02-02 08:35:28 +0300

    chore(ui): Align JWT refresh timing vs axios queue (CI runner)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -18,3 +18,5 @@ function Header({ title }) {
 
 export default Header; 
 // 2023-01-21: Review ProtectedRoute redirect loop guard (local dev)
+
+// 2023-02-02: Align JWT refresh timing vs axios queue (CI runner)
