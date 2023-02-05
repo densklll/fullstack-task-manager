3a55cfd1546f4527ebf09a4251fe57d23e9b5b12
densklll <wqjasabim@mail.ru>
2023-02-05 17:05:04 +0300

    style(serializers): Adjust task status filter query params (prod checklist)

--- backend/apps/tasks/serializers.py
+++ backend/apps/tasks/serializers.py
@@ -8,3 +8,5 @@ class TaskSerializer(serializers.ModelSerializer):
 # 2023-01-13: Capture JWT refresh timing vs axios queue (CI runner)
 
 # 2023-01-26: Review CRACO alias for tests (local dev)
+
+# 2023-02-05: Adjust task status filter query params (prod checklist)
