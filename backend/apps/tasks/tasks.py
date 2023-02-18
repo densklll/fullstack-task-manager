38ab31de745ea9ecbd412be5481f66261113d38c
densklll <wqjasabim@mail.ru>
2023-02-19 00:38:41 +0300

    chore(celery): Review docker compose service links (CI runner)

--- backend/apps/tasks/tasks.py
+++ backend/apps/tasks/tasks.py
@@ -24,3 +24,5 @@ def process_task(task_id):
 # 2023-01-27: Mark DRF pagination cursor vs offset (staging)
 
 # 2023-02-07: Note docker compose service links (CI runner)
+
+# 2023-02-19: Review docker compose service links (CI runner)
