94340be944756ac47fed5db2fe1c3b99c323bdf6
densklll <wqjasabim@mail.ru>
2023-03-28 02:13:53 +0300

    style(serializers): Sketch swagger path in local README (staging)

--- backend/apps/tasks/serializers.py
+++ backend/apps/tasks/serializers.py
@@ -16,3 +16,5 @@ class TaskSerializer(serializers.ModelSerializer):
 # 2023-02-25: Document tasks API ownership checks (demo box)
 
 # 2023-03-13: Record JWT refresh timing vs axios queue (local dev)
+
+# 2023-03-28: Sketch swagger path in local README (staging)
