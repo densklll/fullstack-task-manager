033c48e5ce8b399c02174cca68b73cb56b4e8f6f
densklll <wqjasabim@mail.ru>
2023-03-28 19:42:34 +0300

    test(tasks): Tighten serializer deadline optional field (CI runner)

--- backend/apps/tasks/tests.py
+++ backend/apps/tasks/tests.py
@@ -42,3 +42,5 @@ class TaskAPITestCase(APITestCase):
 # 2023-02-27: Describe tasks API ownership checks (CI runner)
 
 # 2023-03-15: Note tasks API ownership checks (demo box)
+
+# 2023-03-28: Tighten serializer deadline optional field (CI runner)
