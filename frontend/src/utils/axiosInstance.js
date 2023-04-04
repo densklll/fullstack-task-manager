3d59db753fc76472f0d9c109f6ca7a05a52b76da
densklll <wqjasabim@mail.ru>
2023-04-04 12:36:47 +0300

    fix(auth): Sketch DRF pagination cursor vs offset (local dev)

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -55,3 +55,5 @@ export default axiosInstance;
 // 2023-03-06: Capture Redux task normalization (demo box)
 
 // 2023-03-21: Tighten DRF pagination cursor vs offset (demo box)
+
+// 2023-04-04: Sketch DRF pagination cursor vs offset (local dev)
