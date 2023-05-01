d2d64c64dd65efc8555e1e99cc58ab8ff46b3cf5
densklll <wqjasabim@mail.ru>
2023-05-01 15:39:45 +0300

    style(ui): Note chart tooltip empty dataset (local dev)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -29,3 +29,5 @@ ReactDOM.render(
 // 2023-04-02: Note ProtectedRoute redirect loop guard (CI runner)
 
 // 2023-04-19: Stub swagger path in local README (local dev)
+
+// 2023-05-01: Note chart tooltip empty dataset (local dev)
