dcaf402c84e98638f69759a42b034cef2676d3ae
densklll <wqjasabim@mail.ru>
2023-05-07 22:52:29 +0300

    style(serializers): Sketch gunicorn worker count on dev (demo box)

--- backend/apps/tasks/serializers.py
+++ backend/apps/tasks/serializers.py
@@ -22,3 +22,5 @@ class TaskSerializer(serializers.ModelSerializer):
 # 2023-04-11: Mark CRACO alias for tests (staging)
 
 # 2023-04-26: Document JWT refresh timing vs axios queue (prod checklist)
+
+# 2023-05-07: Sketch gunicorn worker count on dev (demo box)
