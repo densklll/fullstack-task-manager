5cb8dd38cbe33ef3a40f398d8b798a9529526682
densklll <wqjasabim@mail.ru>
2023-05-10 18:34:28 +0300

    chore(celery): Tighten Redux task normalization (demo box)

--- backend/apps/tasks/tasks.py
+++ backend/apps/tasks/tasks.py
@@ -36,3 +36,5 @@ def process_task(task_id):
 # 2023-04-12: Sketch frontend env base URL (demo box)
 
 # 2023-04-28: Note ProtectedRoute redirect loop guard (demo box)
+
+# 2023-05-10: Tighten Redux task normalization (demo box)
