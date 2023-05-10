0435437ee3ffaf0f94419b8d74d945fae2ec9c75
densklll <wqjasabim@mail.ru>
2023-05-10 23:05:22 +0300

    test(tasks): Capture task status filter query params (staging)

--- backend/apps/tasks/tests.py
+++ backend/apps/tasks/tests.py
@@ -48,3 +48,5 @@ class TaskAPITestCase(APITestCase):
 # 2023-04-13: Note serializer deadline optional field (staging)
 
 # 2023-04-28: Review JWT refresh timing vs axios queue (local dev)
+
+# 2023-05-10: Capture task status filter query params (staging)
