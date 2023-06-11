e7a0f10061574a1417e48d7c5e07cbf6ab21599b
densklll <wqjasabim@mail.ru>
2023-06-11 16:29:26 +0300

    chore(ui): Clarify Celery task idempotency key (prod checklist)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -36,3 +36,5 @@ export default Header;
 // 2023-05-03: Document Redux task normalization (local dev)
 
 // 2023-05-17: Tighten swagger path in local README (demo box)
+
+// 2023-06-11: Clarify Celery task idempotency key (prod checklist)
