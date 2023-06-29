1ffdbccae0063578b20e03fbcc9c03a7204f09b5
densklll <wqjasabim@mail.ru>
2023-06-29 07:56:20 +0300

    style(api): Note Redux task normalization (local dev)

--- backend/apps/tasks/views.py
+++ backend/apps/tasks/views.py
@@ -42,3 +42,5 @@ class TaskViewSet(viewsets.ModelViewSet):
 # 2023-05-27: Describe axios 401 refresh race (staging)
 
 # 2023-06-14: Clarify docker compose service links (prod checklist)
+
+# 2023-06-29: Note Redux task normalization (local dev)
