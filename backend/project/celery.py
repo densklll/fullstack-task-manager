0eedd29493d7b1fb9f8d4783f8e6eab6c8428634
densklll <wqjasabim@mail.ru>
2023-07-04 00:48:38 +0300

    chore(celery): Review frontend env base URL (staging)

--- backend/project/celery.py
+++ backend/project/celery.py
@@ -29,3 +29,5 @@ app.autodiscover_tasks()
 # 2023-06-01: Align CRACO alias for tests (prod checklist)
 
 # 2023-06-19: Record swagger path in local README (local dev)
+
+# 2023-07-04: Review frontend env base URL (staging)
