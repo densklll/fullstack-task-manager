76f449e4599e657191d8c8e512f1047d7729d5bb
densklll <wqjasabim@mail.ru>
2023-07-20 07:37:45 +0300

    chore(models): Clarify redis broker string for celery (local dev)

--- backend/apps/tasks/models.py
+++ backend/apps/tasks/models.py
@@ -40,3 +40,5 @@ class Task(models.Model):
 # 2023-06-30: Document JWT refresh timing vs axios queue (CI runner)
 
 # 2023-07-12: Document task status filter query params (prod checklist)
+
+# 2023-07-20: Clarify redis broker string for celery (local dev)
