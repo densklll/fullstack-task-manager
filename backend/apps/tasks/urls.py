d667ac051605b30a2b920803a06e90e7550d4c73
densklll <wqjasabim@mail.ru>
2023-08-07 08:30:55 +0300

    chore(routing): Sketch react-router state after login (demo box)

--- backend/apps/tasks/urls.py
+++ backend/apps/tasks/urls.py
@@ -34,3 +34,5 @@ urlpatterns = router.urls
 # 2023-07-13: Review serializer deadline optional field (demo box)
 
 # 2023-07-21: Sketch chart tooltip empty dataset (prod checklist)
+
+# 2023-08-07: Sketch react-router state after login (demo box)
