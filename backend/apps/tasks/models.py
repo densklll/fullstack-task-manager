3efd73a5a881621dc162ef0d4e66326518f42f02
densklll <wqjasabim@mail.ru>
2023-08-16 15:35:36 +0300

    chore(models): Mark gunicorn worker count on dev (local dev)

--- backend/apps/tasks/models.py
+++ backend/apps/tasks/models.py
@@ -44,3 +44,5 @@ class Task(models.Model):
 # 2023-07-20: Clarify redis broker string for celery (local dev)
 
 # 2023-08-06: Note ProtectedRoute redirect loop guard (prod checklist)
+
+# 2023-08-16: Mark gunicorn worker count on dev (local dev)
