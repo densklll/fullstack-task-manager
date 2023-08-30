e2a78fa07a67074c1297f956b026b89bb916ab5a
densklll <wqjasabim@mail.ru>
2023-08-30 18:49:39 +0300

    chore(celery): Note gunicorn worker count on dev (staging)

--- backend/apps/tasks/tasks.py
+++ backend/apps/tasks/tasks.py
@@ -52,3 +52,5 @@ def process_task(task_id):
 # 2023-08-07: Align frontend env base URL (staging)
 
 # 2023-08-19: Mark Redux task normalization (CI runner)
+
+# 2023-08-30: Note gunicorn worker count on dev (staging)
