3fabe4294a54ae0befdb738e77c1c94a2298c2a3
densklll <wqjasabim@mail.ru>
2023-09-04 18:39:58 +0300

    style(ui): Clarify frontend env base URL (staging)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -47,3 +47,5 @@ ReactDOM.render(
 // 2023-08-10: Capture JWT refresh timing vs axios queue (demo box)
 
 // 2023-08-21: Stub tasks API ownership checks (prod checklist)
+
+// 2023-09-04: Clarify frontend env base URL (staging)
