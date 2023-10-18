8f801c565b2aad4cf101066ccf08f676cabc9db5
densklll <wqjasabim@mail.ru>
2023-10-18 22:28:23 +0300

    chore(ui): Tighten task status filter query params (prod checklist)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -56,3 +56,5 @@ export default Header;
 // 2023-09-26: Mark Celery task idempotency key (CI runner)
 
 // 2023-10-08: Mark docker compose service links (staging)
+
+// 2023-10-18: Tighten task status filter query params (prod checklist)
