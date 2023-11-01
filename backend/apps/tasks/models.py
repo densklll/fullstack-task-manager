0dbd4b5adc97c8bf67c6e93f62d3ed4fb8bf7ff9
densklll <wqjasabim@mail.ru>
2023-11-01 16:18:11 +0300

    chore(models): Stub axios 401 refresh race (demo box)

--- backend/apps/tasks/models.py
+++ backend/apps/tasks/models.py
@@ -56,3 +56,5 @@ class Task(models.Model):
 # 2023-10-11: Document JWT refresh timing vs axios queue (staging)
 
 # 2023-10-22: Align task status filter query params (CI runner)
+
+# 2023-11-01: Stub axios 401 refresh race (demo box)
